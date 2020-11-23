d4252e269b1b43632b9ef224bf440929a2918297
Borui Tao <borui.tao@mail.mcgill.ca>
2020-11-22 19:40:45 -0500

    Finished sequential implementation for medium, still need double check.

--- reversi.c
+++ reversi.c
@@ -1,14 +1,7 @@
-/*******************************************************************
- *                                                                 *
- *  Author: rodolfoams                                             *
- *  Contact: rodolfo ( at ) grupomarinho ( dot ) com ( dot ) br    *
- *  (C) 2018 All rights reserved                                   *
- *                                                                 *
- ******************************************************************/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #define WHITE    0
 #define BLACK    1
@@ -65,7 +58,11 @@ int distance( int i1, int j1, int i2, int j2 )
 
 int is_playable( int i, int j )
 {
-    memset( playable_direction[i][j], 0, 8 );
+   // memset( playable_direction[i][j], 0, 8 );
+     for ( int m=0; m<8; ++m )
+    {
+      playable_direction[i][j][m] = 0;
+    }
     if ( !is_valid_position( i, j ) ) return FALSE;
     if ( board[i][j] != EMPTY ) return FALSE;
     int playable = FALSE;
@@ -140,6 +137,7 @@ int is_playable( int i, int j )
     }
     if ( is_valid_position( i_it, j_it ) && distance( i, j, i_it, j_it ) > 1 && board[i_it][j_it] == current_player )
     {
+        printf("%d, %d, %d, %d\n", i, j, i_it, j_it);
         playable_direction[i][j][5] = 1;
         playable = TRUE;
     }
@@ -170,6 +168,117 @@ int is_playable( int i, int j )
     return playable;
 }
 
+int is_playable_dummy( int i, int j, char copy_board[8][8], int copy_playable_direction[8][8][8], int tmp_current_player)
+{
+   // memset( copy_playable_direction[i][j], 0, 8 );
+     for ( int m=0; m<8; ++m )
+    {
+      copy_playable_direction[i][j][m] = 0;
+    }
+    if ( !is_valid_position( i, j ) ) return FALSE;
+    if ( copy_board[i][j] != EMPTY ) return FALSE;
+    int playable = FALSE;
+
+    int opposing_player = ( tmp_current_player + 1 ) % 2;
+    
+    // Test UL diagonal
+    int i_it = i-1, j_it = j-1;
+    while ( is_valid_position( i_it, j_it ) && copy_board[i_it][j_it] == opposing_player )
+    {
+        i_it -= 1;
+        j_it -= 1;
+    }
+    if ( is_valid_position( i_it, j_it ) && distance( i, j, i_it, j_it ) > 1 && copy_board[i_it][j_it] == tmp_current_player )
+    {
+        copy_playable_direction[i][j][0] = 1;
+        playable = TRUE;
+    }
+
+    // Test UP path
+    i_it = i-1, j_it = j;
+    while ( is_valid_position( i_it, j_it ) && copy_board[i_it][j_it] == opposing_player )
+        i_it -= 1;
+
+    if ( is_valid_position( i_it, j_it ) && distance( i, j, i_it, j_it ) > 1 && copy_board[i_it][j_it] == tmp_current_player )
+    {
+        copy_playable_direction[i][j][1] = 1;
+        playable = TRUE;
+    }
+    
+    // Test UR diagonal
+    i_it = i-1, j_it = j+1;
+    while ( is_valid_position( i_it, j_it ) && copy_board[i_it][j_it] == opposing_player )
+    {
+        i_it -= 1;
+        j_it += 1;
+    }
+    if ( is_valid_position( i_it, j_it ) && distance( i, j, i_it, j_it ) > 1 && copy_board[i_it][j_it] == tmp_current_player )
+    {
+        copy_playable_direction[i][j][2] = 1;
+        playable = TRUE;
+    }
+
+    // Test LEFT path
+    i_it = i, j_it = j-1;
+    while ( is_valid_position( i_it, j_it ) && copy_board[i_it][j_it] == opposing_player )
+        j_it -= 1;
+
+    if ( is_valid_position( i_it, j_it ) && distance( i, j, i_it, j_it ) > 1 && copy_board[i_it][j_it] == tmp_current_player )
+    {
+        copy_playable_direction[i][j][3] = 1;
+        playable = TRUE;
+    }
+
+    // Test RIGHT path
+    i_it = i, j_it = j+1;
+    while ( is_valid_position( i_it, j_it ) && copy_board[i_it][j_it] == opposing_player )
+        j_it += 1;
+
+    if ( is_valid_position( i_it, j_it ) && distance( i, j, i_it, j_it ) > 1 && copy_board[i_it][j_it] == tmp_current_player )
+    {
+        copy_playable_direction[i][j][4] = 1;
+        playable = TRUE;
+    }
+
+    // Test DL diagonal
+    i_it = i+1, j_it = j-1;
+    while ( is_valid_position( i_it, j_it ) && copy_board[i_it][j_it] == opposing_player )
+    {
+        i_it += 1;
+        j_it -= 1;
+    }
+    if ( is_valid_position( i_it, j_it ) && distance( i, j, i_it, j_it ) > 1 && copy_board[i_it][j_it] == tmp_current_player )
+    {
+        copy_playable_direction[i][j][5] = 1;
+        playable = TRUE;
+    }
+
+    // Test DOWN path
+    i_it = i+1, j_it = j;
+    while ( is_valid_position( i_it, j_it ) && copy_board[i_it][j_it] == opposing_player )
+        i_it += 1;
+
+    if ( is_valid_position( i_it, j_it ) && distance( i, j, i_it, j_it ) > 1 && copy_board[i_it][j_it] == tmp_current_player )
+    {
+        copy_playable_direction[i][j][6] = 1;
+        playable = TRUE;
+    }
+
+    // Test DR diagonal
+    i_it = i+1, j_it = j+1;
+    while ( is_valid_position( i_it, j_it ) && copy_board[i_it][j_it] == opposing_player )
+    {
+        i_it += 1;
+        j_it += 1;
+    }
+    if ( is_valid_position( i_it, j_it ) && distance( i, j, i_it, j_it ) > 1 && copy_board[i_it][j_it] == tmp_current_player )
+    {
+        copy_playable_direction[i][j][7] = 1;
+        playable = TRUE;
+    }
+    return playable;
+}
+
 void mark_playable_positions( )
 {
     has_valid_move = FALSE;
@@ -182,6 +291,24 @@ void mark_playable_positions( )
             if ( is_playable( i, j ) )
             {
                 board[i][j] = PLAYABLE;
+                has_valid_move = TRUE;
+            }
+        }
+    }
+}
+
+void mark_playable_positions_dummy(char copy_board[8][8], int copy_playable_direction[8][8][8], int tmp_current_player)
+{
+    has_valid_move = FALSE;
+    for ( int i=0; i<8; ++i )
+    {
+        for ( int j=0; j<8; ++j )
+        {
+            if ( copy_board[i][j] == PLAYABLE )
+                copy_board[i][j] = EMPTY;
+            if ( is_playable_dummy( i, j, copy_board, copy_playable_direction, tmp_current_player) )
+            {
+                copy_board[i][j] = PLAYABLE;
                 has_valid_move = TRUE;
             }
         }
@@ -253,7 +380,7 @@ void capture_pieces( int i, int j )
 {
     int opposing_player = ( current_player + 1 ) % 2;
     int i_it, j_it;
-    
+    //printf("capture_pieces: row: %d, column: %d\n", i, j);
     // Capture UL diagonal
     if ( playable_direction[i][j][0] )
     {
@@ -265,6 +392,7 @@ void capture_pieces( int i, int j )
             scores[opposing_player]--;
             i_it -= 1;
             j_it -= 1;
+            if(scores[opposing_player] < 0) printf("problem 1");
         }
     }
 
@@ -278,6 +406,7 @@ void capture_pieces( int i, int j )
             scores[current_player]++;
             scores[opposing_player]--;
             i_it -= 1;
+            if(scores[opposing_player] < 0) printf("problem 2");
         }
     }
     
@@ -292,6 +421,7 @@ void capture_pieces( int i, int j )
             scores[opposing_player]--;
             i_it -= 1;
             j_it += 1;
+            if(scores[opposing_player] < 0) printf("problem 3");
         }
     }
 
@@ -305,6 +435,7 @@ void capture_pieces( int i, int j )
             scores[current_player]++;
             scores[opposing_player]--;
             j_it -= 1;
+            if(scores[opposing_player] < 0) printf("problem 4");
         }
     }
 
@@ -318,6 +449,7 @@ void capture_pieces( int i, int j )
             scores[current_player]++;
             scores[opposing_player]--;
             j_it += 1;
+            if(scores[opposing_player] < 0) printf("problem 5");
         }
     }
 
@@ -332,6 +464,7 @@ void capture_pieces( int i, int j )
             scores[opposing_player]--;
             i_it += 1;
             j_it -= 1;
+            if(scores[opposing_player] < 0) printf("problem 6\n");
         }
     }
 
@@ -345,6 +478,7 @@ void capture_pieces( int i, int j )
             scores[current_player]++;
             scores[opposing_player]--;
             i_it += 1;
+            if(scores[opposing_player] < 0) printf("problem 7");
         }
     }
 
@@ -359,6 +493,7 @@ void capture_pieces( int i, int j )
             scores[opposing_player]--;
             i_it += 1;
             j_it += 1;
+            if(scores[opposing_player] < 0) printf("problem 8");
         }
     }
 }
@@ -471,20 +606,24 @@ int capture_potential_pieces( int i, int j, char copy_board[8][8], int curr_pote
     return potential_score;
 }
 
-// Play to the first playable tile
-void get_dummyAI_move( int *p_row, int *p_column )
+// Random player
+void get_random_move( int *p_row, int *p_column )
 {
+  srand(time(0));
+  int count = 0;
   for ( int i=0; i<8; ++i )
+  {
+    for ( int j=0; j<8; ++j )
     {
-        for ( int j=0; j<8; ++j )
-        {
-          if (board[i][j] == PLAYABLE) {
-            *p_row = i;
-            *p_column = j;
-            return;
-          }
-        }
+      if (board[i][j] == PLAYABLE) {
+        count++;
+        if (rand() % count == 0) {
+          *p_row = i;
+          *p_column = j;
+        }        
+      }
     }
+  }
 }
 
 // Think ahead 1 step
@@ -501,27 +640,68 @@ void get_easyAI_move( int *p_row, int *p_column )
               memcpy(copy_board, board, sizeof (char) * 8 * 8);
               memcpy(copy_playable_direction, playable_direction, sizeof (int) * 8 * 8 * 8);
               int s = capture_potential_pieces(i, j, copy_board, current_player, copy_playable_direction);
-              if (s > maxScore) {
+              if (s >= maxScore) {
                 maxScore = s;
                 *p_row = i;
                 *p_column = j;
+                //printf("current player: %d here\n", current_player);
               }
           }
         }
     }
 }
 
+int predict_next_move(char cboard[8][8], int cplayable_direction[8][8][8], int tmp_current_player, int count){
+    if(count == 0) return 0;
+    int maxScore = -100;
+    for ( int i=0; i<8; ++i ) {
+        for ( int j=0; j<8; ++j ) {
+          if (cboard[i][j] == PLAYABLE) {
+              char copy_board[8][8];
+              int copy_playable_direction[8][8][8];
+              memcpy(copy_board, cboard, sizeof (char) * 8 * 8);
+              memcpy(copy_playable_direction, cplayable_direction, sizeof (int) * 8 * 8 * 8);
+
+              int s = capture_potential_pieces(i, j, copy_board, tmp_current_player, copy_playable_direction);
+
+              mark_playable_positions_dummy(copy_board, copy_playable_direction, (tmp_current_player+1) % 2);
+              int res = predict_next_move(copy_board, copy_playable_direction, (tmp_current_player+1) % 2, count-1);
+
+              s = s - res;
+              if (s >= maxScore) {
+                maxScore = s;
+              }
+          }
+        }
+    }
+    return maxScore;
+}
+
 // Think ahead 3 steps
 void get_mediumAI_move( int *p_row, int *p_column )
 {
+  int maxScore = -1000;
   for ( int i=0; i<8; ++i )
     {
         for ( int j=0; j<8; ++j )
         {
           if (board[i][j] == PLAYABLE) {
-            *p_row = i;
-            *p_column = j;
-            return;
+              char copy_board[8][8];
+              int copy_playable_direction[8][8][8];
+              memcpy(copy_board, board, sizeof (char) * 8 * 8);
+              memcpy(copy_playable_direction, playable_direction, sizeof (int) * 8 * 8 * 8);
+              int s = capture_potential_pieces(i, j, copy_board, current_player, copy_playable_direction);
+              mark_playable_positions_dummy(copy_board, copy_playable_direction, (current_player+1) % 2);
+              int res = predict_next_move(copy_board, copy_playable_direction, (current_player+1) % 2, 2);
+
+              int diff = s - res;
+              printf("medium score: %d here, row: %d, column: %d\n", diff, i, j);
+              if (diff >= maxScore) {
+                maxScore = s;
+                *p_row = i;
+                *p_column = j;
+               // printf("maxScore: %d here\n", maxScore);
+              }
           }
         }
     }
@@ -531,13 +711,16 @@ void make_next_move( )
 {
     int row, column;
     if (AI_PLAYER == current_player) {
-      get_easyAI_move( &row, &column );
+      get_mediumAI_move( &row, &column );
+      printf("medium: row: %d, column: %d\n", row, column);
     } else {
        //prompt_move( &row, &column );
-       get_dummyAI_move( &row, &column );
+       get_random_move( &row, &column );
+       printf("random: row: %d, column: %d\n", row, column);
     }
     if ( is_valid_position( row, column ) && board[row][column] == PLAYABLE )
     {
+       // printf("row: %d, column: %d is valid\n", row, column);
         board[row][column] = current_player;
         scores[current_player]++;
         capture_pieces( row, column );
@@ -571,6 +754,7 @@ int main( )
         {
             if ( skipped_turn )
             {
+
                 game_ended = 1;
                 draw_board( );
                 continue;
@@ -584,7 +768,10 @@ int main( )
         display_score( );
         display_current_player( );
         display_wrong_move( );
+        if(wrong_move) exit(0);
         make_next_move( );
     }
+    //mark_playable_positions();
+   // draw_board( );
     display_winner( );
 }
